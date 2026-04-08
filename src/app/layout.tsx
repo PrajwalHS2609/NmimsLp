82a1f9ddc65ed6bf1a9b96e47ddaafa41b24f2f8
Prajwal H S <prajwalhs4india@gmail.com>
2026-04-08 20:10:20 +0530

    added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from './../Components/Navbar/Navbar';
 import PopupForm from './../Components/PopupForm/PopupForm';
 import WhatsApp from './../Components/WhatsApp/WhatsApp';
 import Topbar from './../Components/Topbar/Topbar';
+import Script from "next/script";
 
 
 
@@ -16,10 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" type="image/png" href="/favicon.ico" />
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17917921555"
+          strategy="afterInteractive"
+        />
 
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17917921555');
+          `}
+        </Script>
+      </head>
       <body >
-        <Topbar/>
+        <Topbar />
         <Navbar />
         {children}
         <Footer />
